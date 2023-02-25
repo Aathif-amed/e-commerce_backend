4226c184dcea32990597a6870eb98839af434ffc
Aathif-amed <aathif@auttvl.ac.im>
2023-02-25 22:48:37 +0530

    updAted

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
-const express = require('express');
-const app= express();
-const http= require('http')
-const server =http.createServer(app);
-const {Server}=require('socket.io');
-const cors=require('cors')
-const port =  process.env.PORT||8080
-const io=new Server(server,{
-    cors:'*',
-    methods:'*'
-})
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const cors = require("cors");
+const port = process.env.PORT || 8080;
+const io = new Server(server, {
+  cors: "*",
+  methods: "*",
+});
+
+//importing routes
+const userRoute = require("./routes/userRoutes");
+const productRoute = require("./routes/productRoutes");
+// const orderRoute = require("./routes/orderRoutes");
+const imageRoute = require("./routes/imageRoutes");
+
+const dotenv = require("dotenv");
+dotenv.config();
+//importing database connnetion
+const connection = require("./utils/db");
+//DB connection
+connection();
 
 app.use(cors());
-app.use(express.urlencoded({extended:true}));
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
 
-app.get("/",(req,res)=>{
-    res.send("Welcome to E-commerce Backend")
-})
+//routes
 
-server.listen(port,()=>{
-    console.log(`Server is listening on Port:${port}`);
-})
+app.use("/api/auth", userRoute);
+app.use("/api/product", productRoute);
+// app.use("/api/order", orderRoute);
+app.use("/api/images", imageRoute);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to E-commerce Backend");
+});
+
+server.listen(port, () => {
+  console.log(`Server is listening on Port:${port}`);
+});
