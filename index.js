7f22227f54dacf7d779d36a1806d03841daa52d7
Aathif-amed <aathif@auttvl.ac.im>
2023-02-27 09:58:49 +0530

    cors added

--- index.js
+++ index.js
@@ -23,7 +23,11 @@ const connection = require("./utils/db");
 //DB connection
 connection();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "10mb" }));
 
